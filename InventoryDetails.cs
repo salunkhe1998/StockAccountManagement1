6aa2c4c791caf6dad9cd8f68e401a6d41062de6f
Ashwini <salunkheashwini1998@gmail.com>
2023-01-13 16:07:37 +0530

    this is file

---
+++ InventoryDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement1
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfStock> typesOfStock;
+    }
+    public class TypesOfStock
+    {
+        public string name;
+        public int NoOfshare;
+        public int price;
+    }
+
+}
+Footer
