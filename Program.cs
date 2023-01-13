9d79b571eec432badcef99366400f466d36ec02d
Ashwini <salunkheashwini1998@gmail.com>
2023-01-13 15:57:26 +0530

    this is file

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using InventoryManagementDeno;
+
+namespace InventoryManagementDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to the Object Oriented Program *****");
+
+            string path = "C:\\Users\\user\\source\\repos\\StockAccountManagement1\\StockAccountManagement1\\Inventory.json";
+            ReadData obj = new ReadData();
+            var Info = obj.Read(path);
+
+            //Storing the multiple values of Array in List.
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Stock is:");
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < Info.typesOfStock.Count; i++)
+            {
+                Console.WriteLine("Name: " + Info.typesOfStock[i].name);
+                Console.WriteLine("NoOfshare: " + Info.typesOfStock[i].NoOfshare);
+                Console.WriteLine("price: " + Info.typesOfStock[i].price);
+
+                int val = Info.typesOfStock[i].NoOfshare * Info.typesOfStock[i].price;
+                Console.WriteLine("The price for " + Info.typesOfStock[i].NoOfshare + " Stock is " + val);
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------");
+
+
+        }
+    }
+}
